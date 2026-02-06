4cd96b8a23bd7c27287cb00d47ad344df172967e
Cannon07 <jayshitre@gmail.com>
2026-02-06 23:35:08 +0530

    Removed PWA functionality for now

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,7 @@
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
-import withPWA from 'next-pwa'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
-const pwa = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -24,4 +16,4 @@ const nextConfig = {
   },
 }
 
-export default pwa(nextConfig)
+export default nextConfig
